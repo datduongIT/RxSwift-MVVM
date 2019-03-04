47f3615a237688c4067ddef55428a2a23c20a796
dat_duong_vn <dat.duong@ascendcorp.com>
2019-03-04 23:38:32 +0700

    Base code

---
+++ RxSwift&MVVM/RxSwift&MVVM/Modules/Home/ViewController/HomeVC.swift
@@ -0,0 +1,17 @@
+//
+//  HomeVC.swift
+//  RxSwift&MVVM
+//
+//  Created by Dat Duong on 2/23/19.
+//  Copyright © 2019 Dat Duong. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ RxSwift&MVVM/RxSwift&MVVM/Networking/Networking.swift
@@ -0,0 +1,9 @@
+//
+//  Networking.swift
+//  RxSwift&MVVM
+//
+//  Created by Dat Duong on 2/23/19.
+//  Copyright © 2019 Dat Duong. All rights reserved.
+//
+
+import Foundation
